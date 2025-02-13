a6dbec3dee87f09f87e14365f0f9141baa0e7500
Abdullah Fazili <66874041+ABD07xx@users.noreply.github.com>
2025-02-13 12:08:30 +0530

    Update run_trader.py

--- run_trader.py
+++ run_trader.py
@@ -7,4 +7,4 @@ from trading_bot import AlgoTrader
 # Use conservative trader
 from conservative_config import CONFIG as conservative_config
 trader = AlgoTrader(conservative_config)
-trader.run() 
+trader.run(continuous_mode=False)  # Run once for GitHub Actions 
